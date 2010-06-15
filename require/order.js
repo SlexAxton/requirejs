8add3e7c80ae3c9c170d0e306fdd256d7c8072fc
Alex Sexton <AlexSexton@gmail.com>
2010-06-15 17:27:53 -0500

    Switched the cache plugin to the order plugin

--- require/order.js
+++ require/order.js
@@ -12,16 +12,16 @@
 
 (function () {
     //A place to hold callback functions
-    require._cache = {};
+    require._order = {};
 
     require.plugin({
-        prefix: "cache",
+        prefix: "order",
 
         /**
          * This callback is prefix-specific, only gets called for this prefix
          */
         require: function (name, deps, callback, context) {
-            //No-op, require never gets the script cache blocks since they aren't executed
+            //No-op, require never gets the script order blocks since they aren't executed
             //We could potentially return the script nodes here, but that could be awkward
         },
 
@@ -31,7 +31,7 @@
          */
         newContext: function (context) {
             require.mixin(context, {
-                cacheWaiting: []
+                orderWaiting: []
             });
         },
 
@@ -42,27 +42,27 @@
             var context = require.s.contexts[contextName],
                 name    = 'c-' + url;
             
-            function cacheCallback() {
+            function orderCallback() {
                 context.loaded[name] = true;
                 require.checkLoaded(contextName);
                 //Remove the script element from the Head
                 this.parentNode.removeChild(this);
             };
             
-            context.cacheWaiting.push({
+            context.orderWaiting.push({
                 name: name
             });
             
             //Attach a script to the dom and remove it once it loads
-            //Set the type of the node to 'script/cache' via Souders EFWS and LABjs methods
-            require.attach(url, contextName, "require/cache", cacheCallback, "script/cache");
+            //Set the type of the node to 'script/order' via Souders EFWS and LABjs methods
+            require.attach(url, contextName, "require/order", orderCallback, "script/order");
         },
 
         /**
          * Called when the dependencies of a module are checked.
          */
         checkDeps: function (name, deps, context) {
-            //No-op, checkDeps never gets these cached items, they are
+            //No-op, checkDeps never gets these orderd items, they are
             //never executed, and will be checked when they're loaded for real
         },
 
@@ -70,7 +70,7 @@
          * Called to determine if a module is waiting to load.
          */
         isWaiting: function (context) {
-            return !!context.cacheWaiting.length;
+            return !!context.orderWaiting.length;
         },
 
         /**
@@ -79,8 +79,8 @@
         orderDeps: function (context) {
             //Clear up state since further processing could
             //add more things to fetch.
-            var i, dep, waitAry = context.cacheWaiting;
-            context.cacheWaiting = [];
+            var i, dep, waitAry = context.orderWaiting;
+            context.orderWaiting = [];
             for (i = 0; (dep = waitAry[i]); i++) {
                 context.defined[dep.name] = dep.value;
             }
